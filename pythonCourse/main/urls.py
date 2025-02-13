6ca986f609b735d6c18213c14a3a483a44d97175
ZIPLXC <t.gibalov@yandex.ru>
2025-02-13 13:08:54 +0300

    url view
    f

--- pythonCourse/main/urls.py
+++ pythonCourse/main/urls.py
@@ -1,21 +1,56 @@
 # ЭТО ОТСЛЕЖИВАТЕЛЬ ПЕРЕХОДОВ ПО ССЫЛКАМ ВНУТРИ МОДУЛЯ
 # ТУТ МЫ УЖЕ КИДАЕМ НА VIEWS по адресам
 
-from django.urls import path
-
 from . import views, backend
-
+from django.urls import path
+from .views import (
+    LoginPageView,
+    StudentStatementView,
+    StudentMenuView,
+    StudentMainView,
+    InspectorStatementView,
+    InspectorMenuView,
+    InspectorMainView,
+    JuryStatementView,
+    JuryMenuView,
+    JuryMainView,
+    AdminPanelUsersView,
+    AdminPanelUserView,
+    AdminPanelManageView,
+    AdminPanelStatementsView,
+    AdminPanelCacheView,
+    AdminPanelView,
+    AdminPanelLogView,
+)
 urlpatterns = [
-    path('', views.login_page),
-    path('adminpanel', views.admin_page),
-    path('login', views.login_page),
 
-    path('menu', views.menu_page),
+    # авторизация
+    path('',  LoginPageView.as_view(), name='login'),
+    path(LoginPageView.url, LoginPageView.as_view(), name='login'),
 
-    # мобильные штуки
+    # Студент
+    path(StudentStatementView.url, StudentStatementView.as_view(), name='student_statment'),
+    path(StudentMenuView.url, StudentMenuView.as_view(), name='student_menu'),
+    path(StudentMainView.url, StudentMainView.as_view(), name='student_main'),
 
-    path('m.login', views.login_mobile_page),
-    path('m.menu', views.menu_mobile_page),
+    # Инспектор
+    path(InspectorStatementView.url, InspectorStatementView.as_view(), name='inspector_statment'),
+    path(InspectorMenuView.url, InspectorMenuView.as_view(), name='inspector_menu'),
+    path(InspectorMainView.url, InspectorMainView.as_view(), name='inspector_main'),
+
+    # Жюри
+    path(JuryStatementView.url, JuryStatementView.as_view(), name='jury_statment'),
+    path(JuryMenuView.url, JuryMenuView.as_view(), name='jury_menu'),
+    path(JuryMainView.url, JuryMainView.as_view(), name='jury_main'),
+
+    # Админпанель
+    path(AdminPanelUsersView.url, AdminPanelUsersView.as_view(), name='adminpanel_users'),
+    path(AdminPanelUserView.url, AdminPanelUserView.as_view(), name='adminpanel_user'),
+    path(AdminPanelManageView.url, AdminPanelManageView.as_view(), name='adminpanel_data'),
+    path(AdminPanelStatementsView.url, AdminPanelStatementsView.as_view(), name='adminpanel_statments'),
+    path(AdminPanelCacheView.url, AdminPanelCacheView.as_view(), name='adminpanel_cache'),
+    path(AdminPanelView.url, AdminPanelView.as_view(), name='adminpanel'),
+    path(AdminPanelLogView.url, AdminPanelLogView.as_view(), name='adminpanel_log'),
 
     # Запросы к бэку
 
--- pythonCourse/main/views.py
+++ pythonCourse/main/views.py
@@ -1,22 +1,96 @@
+from django.views import View
 from django.shortcuts import render
 
-# ТУТ САМИ VIEWS НАШЕГО МОДУЛЯ
+# Авторизация
+class LoginPageView(View):
+    def get(self, request):
+        return render(request, 'login/index.html',{'url': StudentMainView.url,'url2': InspectorMainView.url,'url3': JuryMainView.url,'url4': AdminPanelView.url})
+    url = 'login'
 
+# Студент
+class StudentStatementView(View):
+    def get(self, request):
+        return render(request, 'student/student_statment.html')
 
-def admin_page(request):
-    return render(request, 'adminpanel/index.html')
+    url = 'studentstatment'
 
-def login_page(request):
-    return render(request, 'login/index.html')
+class StudentMenuView(View):
+    def get(self, request):
+        return render(request, 'student/student_menu.html',{'url': StudentStatementView.url})
 
-def menu_page(request):
-    return render(request, 'menu/index.html')
+    url = 'studentmenu'
 
+class StudentMainView(View):
+    def get(self, request):
+        return render(request, 'student/student_main.html',{'url': StudentMenuView.url,'url2': StudentStatementView.url})
 
+    url = 'studentmain'
+# Инспектор
+class InspectorStatementView(View):
+    def get(self, request):
+        return render(request, 'inspector/inspector_statment.html')
+    url = 'inspectorstatment'
 
-def login_mobile_page(request):
-    return render(request, 'login/index_m.html')
+class InspectorMenuView(View):
+    def get(self, request):
+        return render(request, 'inspector/inspector_menu.html',{'url': InspectorStatementView.url})
 
-def menu_mobile_page(request):
-    return render(request, 'menu/index_m.html')
+    url = 'inspectormenu'
+class InspectorMainView(View):
+    def get(self, request):
+        return render(request, 'inspector/inspector_main.html',{'url': InspectorMenuView.url})
 
+    url = 'inspectormain'
+# Жюри
+class JuryStatementView(View):
+    def get(self, request):
+        return render(request, 'jury/jury_statment.html')
+
+    url = 'jurystatment'
+
+class JuryMenuView(View):
+    def get(self, request):
+        return render(request, 'jury/jury_menu.html',{'url': JuryStatementView.url})
+
+    url = 'jurymenu'
+class JuryMainView(View):
+    def get(self, request):
+        return render(request, 'jury/jury_main.html',{'url': JuryMenuView.url})
+
+    url = 'jurymain'
+# Админпанель
+class AdminPanelUsersView(View):
+    def get(self, request):
+        return render(request, 'adminpanel/adminpanel_users.html',{'url': AdminPanelUserView.url})
+
+    url = 'adminpanelusers'
+class AdminPanelUserView(View):
+    def get(self, request):
+        return render(request, 'adminpanel/adminpanel_user.html')
+
+    url = 'adminpaneluser'
+class AdminPanelManageView(View):
+    def get(self, request):
+        return render(request, 'adminpanel/adminpanel_data.html')
+
+    url = 'adminpaneldata'
+class AdminPanelStatementsView(View):
+    def get(self, request):
+        return render(request, 'adminpanel/adminpanel_statments.html')
+
+    url = 'adminpanelstatments'
+class AdminPanelCacheView(View):
+    def get(self, request):
+        return render(request, 'adminpanel/adminpanel_cache.html',{'url': AdminPanelCacheView.url})
+
+    url = 'adminpanelcache'
+class AdminPanelView(View):
+    def get(self, request):
+        return render(request, 'adminpanel/adminpanel.html',{'url': AdminPanelCacheView.url,'url2': AdminPanelStatementsView.url,'url3': AdminPanelManageView.url,'url4': AdminPanelUsersView.url,'url5': AdminPanelUsersView.url,'url6': AdminPanelLogView.url})
+
+    url = 'adminpanel'
+
+class AdminPanelLogView(View):
+    def get(self, request):
+        return render(request, 'adminpanel/adminpanel_log.html',{'url': AdminPanelLogView.url})
+    url = 'adminpanellog'
